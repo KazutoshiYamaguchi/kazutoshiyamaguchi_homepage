bb7654af19ff96aab0eb0a7e93a0e6b7affbc06e
kazutoshi <umaibou300pon@icloud.com>
2022-02-08 19:04:59 +0900

    d

--- src/components/pages/Entrance.tsx
+++ src/components/pages/Entrance.tsx
@@ -21,7 +21,7 @@ export const Entrance = () => {
 
     //1秒後に/homeに画面遷移
     setTimeout(() => {
-      window.location.href = '/kazutoshiyamaguchi_homepage/home';
+      window.location.href = '/home';
     }, 1000);
 
   }
